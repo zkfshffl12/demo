de953572500baddda4270e34984eba3a65bb5198
zkfshffl12 <92373161+zkfshffl12@users.noreply.github.com>
2025-07-15 13:54:31 +0900

    first commit

---
+++ demo/src/main/java/com/example/demo/hleoo.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Log4j2
+public class hleoo {
+    public static void main(String[] args) {
+        int awsd=20;
+        log.info("awsd,{}", awsd);
+
+    }
+}
